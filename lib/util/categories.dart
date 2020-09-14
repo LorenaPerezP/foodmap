ac43ee3f2d16c4fc3b094f9c86d28951306ec66c
LXperez <leslix.perezp@konradlorenz.edu.co>
2020-09-14 12:27:40 -0500

    Ajuste template a foodmap

---
+++ lib/util/categories.dart
@@ -0,0 +1,29 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+List categories = [
+  {
+    "name": "Bebidas",
+    "icon": FontAwesomeIcons.wineBottle,
+    "items": 10
+  },
+  {
+    "name": "Varios",
+    "icon": FontAwesomeIcons.hamburger,
+    "items": 7
+  },
+  {
+    "name": "Algo mas",
+    "icon": FontAwesomeIcons.hotdog,
+    "items": 9
+  },
+  {
+    "name": "Comida Rapida",
+    "icon": FontAwesomeIcons.pizzaSlice,
+    "items": 5
+  },
+  {
+    "name": "Un poco mas",
+    "icon": FontAwesomeIcons.breadSlice,
+    "items": 15
+  },
+];
---
+++ lib/util/foods.dart
@@ -0,0 +1,50 @@
+List<Map> foods = [
+  {
+   // "img": "assets/food7.jpeg",
+    "name": "Pizza"
+  },
+  {
+   // "img": "assets/food7.jpeg",
+    "name": "Pizza"
+  },
+  {
+   // "img": "assets/food7.jpeg",
+    "name": "Pizza"
+  },
+  {
+   // "img": "assets/food7.jpeg",
+    "name": "Pizza"
+  },
+  {
+   // "img": "assets/food7.jpeg",
+    "name": "Pizza"
+  },
+  {
+   // "img": "assets/food7.jpeg",
+    "name": "Pizza"
+  },
+  {
+   // "img": "assets/food7.jpeg",
+    "name": "Pizza"
+  },
+  {
+   // "img": "assets/food7.jpeg",
+    "name": "Pizza"
+  },
+  {
+   // "img": "assets/food7.jpeg",
+    "name": "Pizza"
+  },
+  {
+   // "img": "assets/food7.jpeg",
+    "name": "Pizza"
+  },
+  {
+  // "img": "assets/food7.jpeg",
+    "name": "Pizza"
+  },
+  {
+   // "img": "assets/food7.jpg",
+    "name": "Pizza"
+  },
+];
